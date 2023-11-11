2a0c38a8acd3166de2d27da4750be0ced7216428
ajaku <andyjaku13@gmail.com>
2023-11-11 15:30:12 -0500

    Final version of hash.cpp

--- program-one/hash.cpp
+++ program-one/hash.cpp
@@ -13,15 +13,17 @@ using namespace std;
 
 // The constructor initializes the hash table.
 hashTable::hashTable(int size) {
-  capacity = primes[7]; // largest number
-  for (int i = 0; i < 8; i++) {
-   if (size < primes[i]) {
-    capacity = primes[i];
-    prime_idx = i;
-    break;
-   }
-  }
-  data.resize(capacity);
+
+	capacity = primes[7]; // largest number
+	for (int i = 0; i < 8; i++) {
+
+		if (size < primes[i]) {
+			capacity = primes[i];
+			prime_idx = i;
+			break;
+		}
+	}
+	data.resize(capacity);
 }
 
 // Insert the specified key into the hash table.
@@ -31,171 +33,156 @@ hashTable::hashTable(int size) {
 // 1 if key already exists in hash table,
 // 2 if rehash fails.
 int hashTable::insert(const string &key, void *pv) {
-  if ((double)filled/(double)capacity >= 0.5) {
-    rehash();
-  }
-  int loc = hash(key);
-  if (data[loc].isOccupied) {
-    if (data[loc].key != key) {
-      // collission
-      int i = loc;
-      while (data[i].isOccupied) {
-        if (i >= capacity - 1) {
-          i = -1;
-        }
-        i++;
-      }
-      data[i].key = key;
-      data[i].isOccupied = true;
-      filled++;
-      return 0;
-    }
-    if (data[loc].isDeleted) {
-      int i = loc;
-      while (data[i].isOccupied) {
-        if (i >= capacity - 1) {
-          i = -1;
-        }
-        i++;
-      }
-      data[i].key = key;
-      data[i].isOccupied = true;
-      filled++;
-      return 0;
-    }
-    return 1;
-  }
-  data[loc].key = key;
-  data[loc].isOccupied = true;
-  data[loc].pv = pv;
-  filled++;
-  return 0;
+
+	if ((double)filled/(double)capacity >= 0.5) { rehash(); }
+
+	if (contains(key)) { return 1; }
+
+	int loc = hash(key);
+	int og_loc = loc;
+
+	// A value already exists there (result of linear probe for another item)
+	while (data[loc].isOccupied) {
+		loc++;
+		// Make sure we only loop once
+		if (loc == og_loc) 	 { return -1; }
+		// Add wraparound for loop
+		if (loc >= capacity) { loc = 0; }
+	}
+
+	data[loc].isOccupied = true;
+	data[loc].isDeleted  = false;
+	data[loc].key 		 = key;
+	data[loc].pv  		 = pv;
+	filled++;
+	return 0;
 }
 
 // Check if the specified key is in the hash table.
 // If so, return true; otherwise, return false.
 bool hashTable::contains(const string &key) {
-  // see if value is stored at hash location
-  // see if key matches desired key (collission)
-  // if collission, iterate through entire table to see if value exists
-  //    Note: A value must exist if a collision is true!
-  int loc = hash(key); // Location of key
-  if (!data[loc].isOccupied)  { return false; } // value doesn't exist
 
-  if (data[loc].key == key) { 
-    if (!data[loc].isDeleted) { return true; } else {
-      // Item was deleted, need to see if a non-deleted copy exists
-      int i = loc;
-      while (data[i].isOccupied) {
-        if ((data[i].key == key) && !data[i].isDeleted) {
-          return true;
-        }
-        i++;
-        if (i >= capacity) {
-          i = 0;
-        }
-      }
-    }
-  return false; 
-  }
+	if (findPos(key) >= 0) { return true; }	
 
-  int i = loc;
-  while (data[i].isOccupied) {
-    if (data[i].key == key) {
-      return true;
-    }
-    i++;
-    if (i >= capacity) {
-      i = 0;
-    }
-  }
-  return false;
+	return false;
 }
 
 int hashTable::getCapacity() {
-  return capacity;
+	return capacity;
 }
 
 int hashTable::getFilled() {
-  return filled;
+	return filled;
 }
 
 string hashTable::atLocation(int loc) {
-  return data[loc].key;
+	return data[loc].key;
 }
 
-void* hashTable::getPointer(const string &key, bool *b) {
-  if (!contains(key)) {
-    *b = false;
-    return nullptr;
-  }
+void *hashTable::getPointer(const string &key, bool *b) {
 
-  *b = true;
-  return data[hash(key)].pv;
+	int pos;
+	if ((pos = findPos(key)) == -1) {
+		*b = false;
+		return nullptr;
+	}
+
+	*b = true;
+	return data[pos].pv;
 }
 
 int hashTable::setPointer(const string &key, void *pv) {
-  if (!contains(key)) {
-    return 1;
-  }
 
-  data[hash(key)].pv = pv;
-  return 0;
+	int pos;
+	if ((pos = findPos(key)) == -1) {
+		return 1;
+	}
+
+	data[pos].pv = pv;
+	return 0;
 }
 
 bool hashTable::remove(const string &key) {
-  if (!contains(key)) {
-    return 1;
-  }
 
-  data[hash(key)].isDeleted  = 1;
+	int pos;
+	if ((pos = findPos(key)) == -1) {
+		return 1;
+	}
 
-  return 0;
+	data[pos].isDeleted = 1;
+
+	return 0;
 }
 
 // Private Methods
 
 // The hash function.
 int hashTable::hash(const string &key) {
-  int hashVal = 0;
-  for(int i = 0; i < key.length(); i++) {
-    hashVal = 37 * hashVal + key[i];
-  }
-  hashVal %= capacity; // ensures values are limited to 0-capacity
 
-  if (hashVal < 0) {
-    hashVal += capacity;
-  }
-  return hashVal;
+	int hashVal = 0;
+	for (int i = 0; i < key.length(); i++) {
+		hashVal = 37 * hashVal + key[i];
+	}
+	hashVal %= capacity; // ensures values are limited to 0-capacity
+
+	if (hashVal < 0) { hashVal += capacity; }
+
+	return hashVal;
 }
 
 // Search for an item with the specified key.
 // Return the position if found, -1 otherwise.
+
+/* Just have a single loop as long as the current place is occupied.
+ * If you get to an unoccupied cell, return -1. While the current cell is occupied,
+ * if the key matches and it is not deleted, return the current location (otherwise keep looping)
+ */
+
+// CORRECTED - ASK PROF
 int hashTable::findPos(const string &key) {
-  if (contains(key)) {
-    return hash(key);
-  }
-  else {
-    return -1;
-  }
+
+	int loc = hash(key);
+
+	if (!data[loc].isOccupied) { return -1; }
+
+	int og_loc = loc;
+	while (data[loc].isOccupied) {
+		
+		// Entry exists and hasn't been deleted
+		if ((data[loc].key == key) && !data[loc].isDeleted) { return loc; }
+	
+		loc++;
+
+		// Make sure we only loop once
+		if (loc == og_loc) 	 { return -1; }
+		// Add wraparound for loop
+		if (loc >= capacity) { loc = 0; }
+
+	}
+	return -1;
 }
 
 // The rehash function; makes the hash table bigger.
 bool hashTable::rehash() {
-  prime_idx += 1;
-  filled = 0;
-  if(this->prime_idx >= MAX_IDX) {
-    exit(1);
-  }
-  vector<hashItem> old_vec(capacity); // old capacity
-  old_vec.swap(data);
-  data.clear();
-  capacity = primes[prime_idx];
-  data.resize(capacity);
-  int prev = prime_idx - 1;
-  for (int i = 0; i < primes[prev]; i++) {
-    insert(old_vec[i].key, old_vec[i].pv);
-  }
-  old_vec.clear();
-  return true;
+
+	prime_idx += 1;
+	filled = 0;
+
+	if (this->prime_idx >= MAX_IDX) { exit(1); }
+
+	vector<hashItem> old_vec(capacity); // old capacity
+	old_vec.swap(data);
+	data.clear();
+	capacity = primes[prime_idx];
+	data.resize(capacity);
+
+	int prev = prime_idx - 1;
+	for (int i = 0; i < primes[prev]; i++) {
+		if (old_vec[i].isOccupied && !old_vec[i].isDeleted) {
+			insert(old_vec[i].key, old_vec[i].pv);
+		}
+	}
+
+	old_vec.clear();
+	return true;
 }
