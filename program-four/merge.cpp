e25cdae23e37ff419f50ab2c925c1d48474cd670
ajaku <andyjaku13@gmail.com>
2023-11-29 15:16:32 -0500

    includes completed pseudo code

--- program-four/merge.cpp
+++ program-four/merge.cpp
@@ -27,68 +27,129 @@ void isInterleaved(const string s1, const string s2, const string s3) {
 
         matrix[0][0] = true;
 
-	/*    s3 = ambbnx
+	/*    
+	 * Known Example:
+	 *    s3 = ambbnx
+	 *    s1 = mbn
+	 *    s2 = abx
+	 *    
+	 *      a m b b n x
+	 * s1 = m b n
+	 * s2 = a b x   
+	 * r = 0; c = 0;
 	 *
+	 *      m b b n x
+	 *
+	 * s1 = m b n
+	 * s2 = b x   
+	 * r = 1; c = 0; 
+	 *
+	 *      b b n x
+	 * s1 = b n
+	 * s2 = b x   
+	 * r = 1; c = 1;
+	 * (Can branch both ways from here!)
+	 *
+	 *      b n x  or      b n x
+	 * s1 = b n	  s1 = n
+	 * s2 = x         s2 = b x 
+	 * r = 2; c = 1;  r = 1; c = 2;
+	 *
+	 *      n x  or        n x
+	 * s1 = n	  s1 = n
+	 * s2 = x         s2 = x 
+	 * r = 2; c = 2;  r = 2; c = 2; 
+	 *
+	 *      x  or          x
+	 * s1 = 	  s1 = 
+	 * s2 = x         s2 = x 
+	 * r = 2; c = 3;  r = 2; c = 3; 
+	 *
+	 *        or          
+	 * s1 = 	  s1 = 
+	 * s2 =           s2 =  
+	 * r = 3; c = 3;  r = 3; c = 3; 
+	 *
+	 * MATCH!
+	 *
+	 *       1 2 3
 	 *    s1 m b n
 	 *  s2 T 
-	 *   a	      
-	 *   b
-	 *   x 
+	 * 1 a T T T	     
+	 * 2 b   T T T
+	 * 3 x       T
+	 *
+	 *
+	 *       1 2 3
+	 *    s1 m b n
+	 *  s2 T F F F
+	 * 1 a T T T F	       
+	 * 2 b F T T T
+	 * 3 x F F F T
+	 *
+	 * Sable Example:
+	 *
+	 *    s3 = ababacd 
+	 *    s1 = abac 
+	 *    s2 = bad
+	 *    
+	 *      a b a b a c d
+	 * s1 = a b a c   
+	 * s2 = b a d 
+	 *
+	 *      b a b a c d
+	 * s1 = b a c   
+	 * s2 = b a d 
+	 * (BRANCH!)
+	 *
+	 *      a b a c d            a b a c d
+	 * s1 = a c   		s1 = b a c
+	 * s2 = b a d		s2 = a d
+	 *
+	 *      b a c d              b a c d
+	 * s1 = c   		s1 = b a c
+	 * s2 = b a d		s2 = d
+	 *
+	 *
+	 *      a c d                a c d
+	 * s1 = c   		s1 = b a c
+	 * s2 = a d		s2 = 
+	 * 			(BRANCH IS DEAD)
+	 *
+	 *      c d            
+	 * s1 = c   		
+	 * s2 = d	
+	 *
+	 *      d            
+	 * s1 =   		
+	 * s2 = d	
+	 *
+	 * MATCH!
+	 *
+	 *
+	 *       1 2 3 4
+	 *    s1 a b a c 
+	 *  s2 T 
+	 * 1 b 	       
+	 * 2 a 
+	 * 3 d
+	 *
+	 *   First iterate through characters of s1,
+	 *   see if they are equal to respective char at idx-1
+	 *   and if the previous token (to the left) was true
+	 *
+	 *   Next iterate through characters of s2,
+	 *   repeat same process above but for columns
 	 *
 	 */
 	
-	// Initialization Step
-	for (int r = 1; r <= s1_len; r++) {
-		if(s3[r-1] == s1[r-1]) {
-			matrix[0][r] = matrix[0][r-1];
-		}
-	}
-	for (int c = 1; c <= s2_len; c++) {
-		if(s3[c-1] == s2[c-1]) {
-			matrix[c][0] = matrix[c-1][0];
-		}
-	}
-        for (int r = 1; r <= s1_len; r++) {
-            for (int c = 1; c <= s2_len; c++) {
-
-		    if (s3[r+c-1] == s2[r-1]) {
-			matrix[r][c] = matrix[r-1][c];
-		    }
-
-		    if (s3[r+c-1] == s1[c-1]) {
-		    	if (!matrix[r][c]) { 
-				matrix[r][c] = matrix[r][c-1]; 
-			}
-		    }
-
-		}
-        }
-
-	// Print out dynamic programming table for easier debugging
-
-	cout << "\n  ";
-
-	for (int i = 0; i < s1_len; i++) {
-		cout << s1[i] << " ";
-	}
-	cout << "\n";
-	for (int r = 0; r < s2_len; r++) {
-		cout << s2[r] << " ";
-		for (int c = 0; c < s1_len; c++) {
-			cout << matrix[r][c] << " ";
-		}
-		cout << "\n";
-	}
-	cout << "\n";
 }
 
 int main() {
-	// a -> b -> c
-	const string s1 = "chocolate";
-	// x -> y -> z
-	const string s2 = "chips";
-	// Would be a valid version
-	const string concat = "cchocholaiptes";
+	const string s1 = "abac";
+	const string s2 = "bad";
+	const string concat = "ababacd";
+	// ABAbaCd should be final output
 	isInterleaved(s1, s2, concat);
 	return 0;
 }
